941464f258d32e820d5afd99637c949fea41cf6b
Goran Vuljar <gogostrash@outlook.com>
2015-11-21 18:43:09 +0100

    gotovo

--- ToString/ToString.cs
+++ ToString/ToString.cs
@@ -19,6 +19,20 @@ namespace Vsite.CSharp
 
         // TODO: Implementirati metodu ToString tako da vraća niz u obliku: "2+3j"
         // (ako je implementacija ispravna, metoda Main bi se trebala izvesti bez problema)
+        public override string ToString()
+        {
+            if(Realni>0 && Imaginarni>0)
+              return string.Format("{0}+{1}j", Realni,Imaginarni);
+            if(Realni>0 && Imaginarni<0)
+                return string.Format("{0}{1}j", Realni, Imaginarni);
+            if (Realni == 0 && Imaginarni < 0)
+                return string.Format("{0}j", Imaginarni);
+            if (Realni == 0 && Imaginarni == 0)
+                return"0";
+            if (Realni< 0 && Imaginarni == 0)
+                return string.Format("{0}", Realni);
+            return "";
+        }
 
 
     }
